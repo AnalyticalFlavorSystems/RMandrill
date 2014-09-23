42d531e4e1e0a77152abd9db400079c2502f7051
Evan Farrell <evandev@gastrograph.com>
2014-09-23 19:20:10 -0400

    First commit

---
+++ R/RMandrill-package.r
@@ -0,0 +1,5 @@
+#' RMandrill.
+#'
+#' @name RMandrill
+#' @docType package
+NULL
---
+++ R/send.R
@@ -0,0 +1,28 @@
+require(jsonlite)
+require(httr)
+
+#' Send an email with template
+#'
+#' This sends an email with template
+#'
+#' @param api_key Your Stripe API Key
+#'
+#' @param args An optional list which can contain an amount to refund
+#'
+#' @return nothing
+#'
+#' @export
+#'
+mandrill_send_template <- function(api_key, template_name, content, to_email, sender) {
+  to <- data.frame(email=recipient)
+  sendData <- list(key=api_key, template_name=template_name, 
+                   template_content=content, message=list(to=to, from_email=sender))
+  link <- "https://mandrillapp.com/api/1.0/messages/send-template.json"
+  jsonData <- toJSON(sendData, auto_unbox=TRUE)
+  .post(link, jsonData)
+}
+
+.post <- function(link, data) {
+    res <- POST(url = link, user_agent("Mandrill-Curl/1.0"), body=data)
+    content(res)
+}
